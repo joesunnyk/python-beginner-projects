75a0b5795ffbcc1137eb94015a04d3ecbbe003f0
Joe <joesunnyk@gmail.com>
2026-06-27 23:09:17 +0530

    Added Resume practice 2

--- 01_escape_sequence_practice/escape_sequence_challenge2.py
+++ 01_escape_sequence_practice/escape_sequence_challenge2.py
@@ -29,3 +29,32 @@ the world has ever seen.
 \n\t\t\tName:
       
 """)
+
+#Resume practice 2
+
+print("\nResume Practice 2")
+
+print("________________________________________")
+print("\n\tJoe Sunny Kondala")
+print("120/3/9/a, Om Sri Sai Nilayam, Kondapur, \nEmail: joesunnyk@gmail.com, Ph: 9177420427", sep= "| ")
+print("________________________________________\n\n")
+print("\tObjective\n")
+print("I'm on a mission to helping younng Indians \nbecome super skilled in christ and \nbecome financially independent so that \nthey can take care of their families \nand help humanity.\n\n")
+print("Technical Skills:\n")
+print("\tPython\n")
+print("\tSQL\n")
+print("\tData Structures\n\n")
+print("Educational qualifications:\n")
+print("\tBachelors Degree\n")
+print("\tIntermediate\n")
+print("\tTenth Class\n\n")
+print("Leisure Activities\n")
+print("\tReading books\n")
+print("\tWatching movies\n")
+print("\tWriting code\n\n")
+print("\t\tDeclaration\n")
+print("I hereby declare that the above\nmentined information is true\nto my knowledge.\n If it is confined,\n I'm liable to any action.\n\n")
+print("Date:\n")
+print("Place:\n")
+print("\t\t\tName:\n\n")
+print("___________________________________________")
